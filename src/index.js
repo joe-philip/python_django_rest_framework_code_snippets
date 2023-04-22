27d24390d5d4924d780279cf1083022c1a5fb5b0
Joe Philip <joe.philip@hotmail.co.in>
2023-04-22 16:32:22 +0530

    added MainHeading component to root

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainHeading from './components/main_heading/component';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <MainHeading />
   </React.StrictMode>
 );
 
