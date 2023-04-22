a862fe17e5954641e4861c5138eaaf87fa23c7a5
Joe Philip <joe.philip@hotmail.co.in>
2023-04-22 17:05:43 +0530

    added components to root component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Description from './components/description/component';
+import Features from './components/features/component';
+import Installation from './components/installation/component';
 import MainHeading from './components/main_heading/component';
+import Requirements from './components/requirements/component';
+import Usage from './components/usage/component';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MainHeading />
+    <Description />
+    <Features />
+    <Requirements />
+    <Installation />
+    <Usage />
   </React.StrictMode>
 );
 
